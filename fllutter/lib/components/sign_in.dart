bf5ef2377f6e4bdc150583805df1403de572e3a4
taniaolivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-29 14:29:48 +0200

    modification de storage pour le jwt

--- fllutter/lib/components/sign_in.dart
+++ fllutter/lib/components/sign_in.dart
@@ -1,146 +1,119 @@
 import 'package:flutter/material.dart';
-import 'package:fllutter/components/sign_up.dart';
 import 'package:fllutter/components/geolocalisation.dart';
 import 'package:fllutter/src/auth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'dart:convert' show json, base64, ascii;
 
-class Connexion extends StatelessWidget {
-  static const String _title = 'Connexion';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const _StateConnection(),
-      ),
-    );
-  }
-}
-
-class _StateConnection extends StatefulWidget {
-  const _StateConnection({Key? key}) : super(key: key);
+class Connection extends StatefulWidget {
+  const Connection({Key? key}) : super(key: key);
 
   @override
-  State<_StateConnection> createState() => _StateConnectionWidget();
+  ConnectionState createState() => ConnectionState();
 }
 
-class _StateConnectionWidget extends State<_StateConnection> {
+class ConnectionState extends State<Connection> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 40, right: 40),
-              width: double.infinity,
-              alignment: Alignment.center,
-              child: const Text(
-                'Connexion',
-                style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 30),
-              ),
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Connexion'),
+              backgroundColor: Colors.blue[600],
             ),
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Remplissez ce champ';
-                  }
-                  return null;
-                },
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Login',
-                  hintText: ' Ex: admin',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Remplissez ce champ';
-                  }
-                },
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Mot de passe',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text(
-                'Mot de passe oublié',
-              ),
-            ),
-            Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: ElevatedButton(
-                child: Text("Valider"),
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    var username = nameController.text;
-                    var password = passwordController.text;
-                    var jwt = await logIn(username, password);
-                    var storage = FlutterSecureStorage();
-                    if (jwt != null) {
-                      print(jwt['user']['token']);
-                      storage.write(key: "jwt", value: jwt['user']['token']);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              Geolocalisation.fromBase64(jwt['user']['token']),
+            body: Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ListView(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(10),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Remplissez ce champ';
+                          }
+                          return null;
+                        },
+                        controller: nameController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Login',
+                          hintText: ' Ex: admin',
                         ),
-                      );
-                    }
-                  }
-                },
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextFormField(
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Remplissez ce champ';
+                          }
+                        },
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Mot de passe',
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        //forgot password screen
+                      },
+                      child: const Text(
+                        'Mot de passe oublié',
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: ElevatedButton(
+                        child: Text("Valider"),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            var username = nameController.text;
+                            var password = passwordController.text;
+                            var jwt = await logIn(username, password);
+                            var storage = FlutterSecureStorage();
+                            if (jwt != null) {
+                              storage.write(
+                                  key: "token", value: jwt['user']['token']);
+                              storage.write(
+                                  key: "id", value: jwt['user']['id']);
+                              Navigator.pushNamed(context, '/geolocalisation');
+                            }
+                          }
+                        },
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        const Text("Pas de compte?"),
+                        TextButton(
+                          child: const Text(
+                            "S'inscrire",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                            // Vue inscription
+                          },
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Row(
-              children: <Widget>[
-                const Text("Pas de compte?"),
-                TextButton(
-                  child: const Text(
-                    "S'inscrire",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                    // Vue inscription
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        ),
-      ),
-    );
+            )));
   }
 }
