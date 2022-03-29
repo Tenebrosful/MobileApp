7532f8137f65bcda2b5236b86bb3dd6b775ad71f
Ziyi WANG <ziyiwang1027@gmail.com>
2022-03-29 20:19:09 +0200

    Fix main : l'affichage profil

--- fllutter/lib/main.dart
+++ fllutter/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:fllutter/components/commentaires.dart';
 import 'package:fllutter/components/geolocalisation.dart';
 import 'package:fllutter/components/one_event.dart';
+import 'package:fllutter/components/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:fllutter/components/sign_in.dart' as connection;
-import 'components/profil.dart';
 import 'components/creation.dart';
 import 'service/service_user.dart';
 import 'service/service_participant.dart';
@@ -31,11 +31,9 @@ class MyApp extends StatelessWidget {
         // When navigating to the "/connexion" route, build the SignIn widget.
         '/connexion': (context) => connection.Connection(),
         // When navigating to the "/profil" route, build the Profil widget.
-        '/profil': (context) => Profil(),
+        '/profil': (context) => ProfilePage(),
         // When navigating to the "/ajout" route, build the AddEven widget.
         '/ajout': (context) => AddEven(),
-        // When navigating to the "/users" route, build the Service_user widget.
-        '/users': (context) => Service_user(),
         // When navigating to the "/events" route, build the Service_event widget.
         '/geolocalisation': (context) => Geolocalisation(),
         // When navigating to the "/participants" route, build the Service_participant widget.
@@ -44,42 +42,7 @@ class MyApp extends StatelessWidget {
         '/evenement': (context) => OneEvent(),
         // When navigating to the "/commentaires" route, build the Commentaires widget.
         '/commentaires': (context) => Commentaires(),
-        //'/commentaire': (context) => Service_comment(),
-        '/auth': (context) => Service_auth(),
       },
     );
   }
 }
-
-
-
-/*
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  static final String title = 'User Profile';
-
-  @override
-  Widget build(BuildContext context) {
-    final user = UserPreferences.myUser;
-    return ThemeProvider(
-      initTheme: user.isDarkMode ? MyThemes.darkTheme : MyThemes.lightTheme,
-      builder: (context, myTheme) {
-        return MaterialApp(
-          title: title,
-          theme: myTheme,
-          home: ProfilePage(),
-        );
-      },
-    );
-  }
-}
-*/
