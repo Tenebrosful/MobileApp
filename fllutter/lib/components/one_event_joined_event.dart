03293c0d663467b95083a6ab17c4e4682a4d57ec
taniaolivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-31 01:05:02 +0200

    modification la partie participants

--- fllutter/lib/components/one_event_joined_event.dart
+++ fllutter/lib/components/one_event_joined_event.dart
@@ -2,19 +2,33 @@ import 'package:fllutter/model-api/joined_events.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fllutter/model-api/join_event.dart' as join;
+import 'package:fllutter/model-api/event_participants.dart' as participants;
 
-class OneEventJoinedEvent extends StatelessWidget {
+class OneEventJoinedEvent extends StatefulWidget {
+  const OneEventJoinedEvent({Key? key}) : super(key: key);
+
+  @override
+  State<OneEventJoinedEvent> createState() => OneEventJoinedEventState();
+}
+
+class OneEventJoinedEventState extends State<OneEventJoinedEvent> {
   String? token;
+  String? owner_id;
+  final storage = FlutterSecureStorage();
+  late final Future<participants.Participants> participants_data;
 
   @override
   Widget build(BuildContext context) {
     final event = ModalRoute.of(context)!.settings.arguments as Event;
-    final participants = event.participants;
     final storage = FlutterSecureStorage();
 
-    Future<void> _joinEvents() async {
+    Future<participants.Participants> getParticipants() async {
+      owner_id = await storage.read(key: "id");
       token = await storage.read(key: "token");
+      return participants.fetchEventParticipants(token.toString(), event.id!);
     }
+
+    participants_data = getParticipants();
 
     return Scaffold(
         appBar: AppBar(
@@ -25,51 +39,63 @@ class OneEventJoinedEvent extends StatelessWidget {
           Column(
             children: <Widget>[
               Container(
-                  constraints: BoxConstraints(
-                    maxHeight: double.infinity,
-                  ),
-                  padding: EdgeInsets.fromLTRB(25, 30, 25, 50),
-                  color: Colors.blue[50],
-                  child: Column(children: <Widget>[
-                    Row(children: [
-                      Icon(Icons.location_on, color: Colors.blue),
-                      SizedBox(width: 20),
-                      Container(
-                        width: 280,
-                        child: Text(
-                          event.coords.address.toString(),
-                          style: TextStyle(fontSize: 17),
-                          textAlign: TextAlign.justify,
-                        ),
-                      ),
-                    ]),
-                    SizedBox(height: 20),
-                    Row(children: <Widget>[
-                      Icon(Icons.person, color: Colors.yellow.shade700),
-                      SizedBox(width: 20),
-                      Text(
-                        event.owner.username.toString(),
-                        style: TextStyle(fontSize: 17),
-                      ),
-                    ]),
-                    SizedBox(height: 20),
-                    Row(children: <Widget>[
-                      Icon(Icons.groups, color: Colors.grey),
-                      SizedBox(width: 20),
-                      Text(
-                        event.comingParticipant.toString() + ' participant(s)',
-                        style: TextStyle(fontSize: 17),
-                      ),
-                    ]),
-                    SizedBox(height: 30),
-                    Container(
-                      child: Text(
-                        event.description.toString(),
-                        textAlign: TextAlign.justify,
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    )
-                  ])),
+                constraints: BoxConstraints(
+                  maxHeight: double.infinity,
+                ),
+                padding: EdgeInsets.fromLTRB(25, 30, 25, 50),
+                color: Colors.blue[50],
+                child: FutureBuilder<participants.Participants>(
+                  future: participants_data,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(children: <Widget>[
+                        Row(children: [
+                          Icon(Icons.location_on, color: Colors.blue),
+                          SizedBox(width: 20),
+                          Container(
+                            width: 280,
+                            child: Text(
+                              event.coords.address.toString(),
+                              style: TextStyle(fontSize: 17),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ),
+                        ]),
+                        SizedBox(height: 20),
+                        Row(children: <Widget>[
+                          Icon(Icons.person, color: Colors.yellow.shade700),
+                          SizedBox(width: 20),
+                          Text(
+                            event.owner.username.toString(),
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ]),
+                        SizedBox(height: 20),
+                        Row(children: <Widget>[
+                          Icon(Icons.groups, color: Colors.grey),
+                          SizedBox(width: 20),
+                          Text(
+                            event.comingParticipant.toString() +
+                                ' participant(s)',
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ]),
+                        SizedBox(height: 30),
+                        Container(
+                          child: Text(
+                            event.description.toString(),
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        )
+                      ]);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+                    return const CircularProgressIndicator();
+                  },
+                ),
+              ),
               Container(
                 color: Colors.blue[50],
                 padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
@@ -81,8 +107,22 @@ class OneEventJoinedEvent extends StatelessWidget {
                         primary: Colors.green.shade400,
                       ),
                       onPressed: () async {
-                        await _joinEvents();
-                        await join.joinEvent(token.toString(), event.id!, true);
+                        final snackBar = SnackBar(
+                          content: const Text('Vous voulez venir ?'),
+                          action: SnackBarAction(
+                            label: 'Oui',
+                            onPressed: () {
+                              join.joinEvent(token.toString(), event.id!, true);
+                              Navigator.pop(context);
+                              setState(() {
+                                participants_data =
+                                    participants.fetchEventParticipants(
+                                        token.toString(), event.id!);
+                              });
+                            },
+                          ),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
                       },
                       child: Text(
                         "Je viens",
@@ -96,9 +136,23 @@ class OneEventJoinedEvent extends StatelessWidget {
                         primary: Colors.red.shade400,
                       ),
                       onPressed: () async {
-                        await _joinEvents();
-                        await join.joinEvent(
-                            token.toString(), event.id!, false);
+                        final snackBar = SnackBar(
+                          content: const Text('Vous ne voulez pas venir ?'),
+                          action: SnackBarAction(
+                            label: 'Non',
+                            onPressed: () {
+                              join.joinEvent(
+                                  token.toString(), event.id!, false);
+                              Navigator.pop(context);
+                              setState(() {
+                                participants_data =
+                                    participants.fetchEventParticipants(
+                                        token.toString(), event.id!);
+                              });
+                            },
+                          ),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
                       },
                       child: Text(
                         "Désolé(e)",
@@ -118,25 +172,37 @@ class OneEventJoinedEvent extends StatelessWidget {
                   child: Container(
                     color: Colors.green[50],
                     padding: EdgeInsets.fromLTRB(25, 20, 20, 25),
-                    child: ListView(
-                      children: participants.map((participant) {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(children: [
-                            Text(participant.username.toString(),
-                                style: TextStyle(
-                                    fontSize: 17, fontWeight: FontWeight.bold)),
-                            SizedBox(width: 6),
-                            Text(
-                                participant.comeToEvent == true
-                                    ? "sera présent(e)"
-                                    : "a dit non",
-                                style: TextStyle(
-                                  fontSize: 17,
-                                ))
-                          ]),
-                        );
-                      }).toList(),
+                    child: FutureBuilder<participants.Participants>(
+                      future: participants_data,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          var participants = snapshot.data!.participants;
+                          return ListView(
+                            children: participants.map((participant) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(children: [
+                                  Text(participant.username.toString(),
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold)),
+                                  SizedBox(width: 6),
+                                  Text(
+                                      participant.comeToEvent == true
+                                          ? "sera présent(e)"
+                                          : "a dit non",
+                                      style: TextStyle(
+                                        fontSize: 17,
+                                      ))
+                                ]),
+                              );
+                            }).toList(),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text('${snapshot.error}');
+                        }
+                        return const CircularProgressIndicator();
+                      },
                     ),
                   ),
                 ),
--- fllutter/lib/components/one_event_self_event.dart
+++ fllutter/lib/components/one_event_self_event.dart
@@ -1,3 +1,4 @@
+import 'package:fllutter/model-api/event_participants.dart' as participants;
 import 'package:fllutter/model-api/self_events.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -13,30 +14,22 @@ class OneEventSelfEvent extends StatefulWidget {
 class OneEventSelfEventState extends State<OneEventSelfEvent> {
   String? token;
   String? username;
+  String? owner_id;
   final storage = FlutterSecureStorage();
-  Future<void> getUsername() async {
-    username = await storage.read(key: "username");
-  }
-
-  @override
-  void initState() {
-    getUsername().then((username) {
-      setState(() {
-        username = username;
-      });
-    });
-    super.initState();
-  }
+  late final Future<participants.Participants> participants_data;
 
   @override
   Widget build(BuildContext context) {
     final event = ModalRoute.of(context)!.settings.arguments as Event;
-    final participants = event.participants;
 
-    Future<void> _joinEvents() async {
+    Future<participants.Participants> getParticipants() async {
+      owner_id = await storage.read(key: "id");
       token = await storage.read(key: "token");
+      username = await storage.read(key: "username");
+      return participants.fetchEventParticipants(token.toString(), event.id!);
     }
 
+    participants_data = getParticipants();
     return Scaffold(
         appBar: AppBar(
           title: Text(event.title.toString()),
@@ -46,51 +39,63 @@ class OneEventSelfEventState extends State<OneEventSelfEvent> {
           Column(
             children: <Widget>[
               Container(
-                  constraints: BoxConstraints(
-                    maxHeight: double.infinity,
-                  ),
-                  padding: EdgeInsets.fromLTRB(25, 30, 25, 50),
-                  color: Colors.blue[50],
-                  child: Column(children: <Widget>[
-                    Row(children: [
-                      Icon(Icons.location_on, color: Colors.blue),
-                      SizedBox(width: 20),
-                      Container(
-                        width: 280,
-                        child: Text(
-                          event.coords.address.toString(),
-                          style: TextStyle(fontSize: 17),
-                          textAlign: TextAlign.justify,
-                        ),
-                      ),
-                    ]),
-                    SizedBox(height: 20),
-                    Row(children: <Widget>[
-                      Icon(Icons.person, color: Colors.yellow.shade700),
-                      SizedBox(width: 20),
-                      Text(
-                        username.toString(),
-                        style: TextStyle(fontSize: 17),
-                      ),
-                    ]),
-                    SizedBox(height: 20),
-                    Row(children: <Widget>[
-                      Icon(Icons.groups, color: Colors.grey),
-                      SizedBox(width: 20),
-                      Text(
-                        event.comingParticipant.toString() + ' participant(s)',
-                        style: TextStyle(fontSize: 17),
-                      ),
-                    ]),
-                    SizedBox(height: 30),
-                    Container(
-                      child: Text(
-                        event.description.toString(),
-                        textAlign: TextAlign.justify,
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    )
-                  ])),
+                constraints: BoxConstraints(
+                  maxHeight: double.infinity,
+                ),
+                padding: EdgeInsets.fromLTRB(25, 30, 25, 50),
+                color: Colors.blue[50],
+                child: FutureBuilder<participants.Participants>(
+                  future: participants_data,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(children: <Widget>[
+                        Row(children: [
+                          Icon(Icons.location_on, color: Colors.blue),
+                          SizedBox(width: 20),
+                          Container(
+                            width: 280,
+                            child: Text(
+                              event.coords.address.toString(),
+                              style: TextStyle(fontSize: 17),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ),
+                        ]),
+                        SizedBox(height: 20),
+                        Row(children: <Widget>[
+                          Icon(Icons.person, color: Colors.yellow.shade700),
+                          SizedBox(width: 20),
+                          Text(
+                            username.toString(),
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ]),
+                        SizedBox(height: 20),
+                        Row(children: <Widget>[
+                          Icon(Icons.groups, color: Colors.grey),
+                          SizedBox(width: 20),
+                          Text(
+                            event.comingParticipant.toString() +
+                                ' participant(s)',
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ]),
+                        SizedBox(height: 30),
+                        Container(
+                          child: Text(
+                            event.description.toString(),
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        )
+                      ]);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+                    return const CircularProgressIndicator();
+                  },
+                ),
+              ),
               Container(
                 color: Colors.blue[50],
                 padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
@@ -101,9 +106,23 @@ class OneEventSelfEventState extends State<OneEventSelfEvent> {
                       style: ElevatedButton.styleFrom(
                         primary: Colors.green.shade400,
                       ),
-                      onPressed: () async {
-                        await _joinEvents();
-                        await join.joinEvent(token.toString(), event.id!, true);
+                      onPressed: () {
+                        final snackBar = SnackBar(
+                          content: const Text('Vous voulez venir ?'),
+                          action: SnackBarAction(
+                            label: 'Oui',
+                            onPressed: () {
+                              join.joinEvent(token.toString(), event.id!, true);
+                              Navigator.pop(context);
+                              setState(() {
+                                participants_data =
+                                    participants.fetchEventParticipants(
+                                        token.toString(), event.id!);
+                              });
+                            },
+                          ),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
                       },
                       child: Text(
                         "Je viens",
@@ -116,10 +135,24 @@ class OneEventSelfEventState extends State<OneEventSelfEvent> {
                       style: ElevatedButton.styleFrom(
                         primary: Colors.red.shade400,
                       ),
-                      onPressed: () async {
-                        await _joinEvents();
-                        await join.joinEvent(
-                            token.toString(), event.id!, false);
+                      onPressed: () {
+                        final snackBar = SnackBar(
+                          content: const Text('Vous ne voulez pas venir ?'),
+                          action: SnackBarAction(
+                            label: 'Non',
+                            onPressed: () {
+                              join.joinEvent(
+                                  token.toString(), event.id!, false);
+                              Navigator.pop(context);
+                              setState(() {
+                                participants_data =
+                                    participants.fetchEventParticipants(
+                                        token.toString(), event.id!);
+                              });
+                            },
+                          ),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
                       },
                       child: Text(
                         "Désolé(e)",
@@ -139,25 +172,37 @@ class OneEventSelfEventState extends State<OneEventSelfEvent> {
                   child: Container(
                     color: Colors.green[50],
                     padding: EdgeInsets.fromLTRB(25, 20, 20, 25),
-                    child: ListView(
-                      children: participants.map((participant) {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(children: [
-                            Text(participant.username.toString(),
-                                style: TextStyle(
-                                    fontSize: 17, fontWeight: FontWeight.bold)),
-                            SizedBox(width: 6),
-                            Text(
-                                participant.comeToEvent == true
-                                    ? "sera présent(e)"
-                                    : "a dit non",
-                                style: TextStyle(
-                                  fontSize: 17,
-                                ))
-                          ]),
-                        );
-                      }).toList(),
+                    child: FutureBuilder<participants.Participants>(
+                      future: participants_data,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          var participantss = snapshot.data!.participants;
+                          return ListView(
+                            children: participantss.map((participant) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(children: [
+                                  Text(participant.username.toString(),
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold)),
+                                  SizedBox(width: 6),
+                                  Text(
+                                      participant.comeToEvent == true
+                                          ? "sera présent(e)"
+                                          : "a dit non",
+                                      style: TextStyle(
+                                        fontSize: 17,
+                                      ))
+                                ]),
+                              );
+                            }).toList(),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text('${snapshot.error}');
+                        }
+                        return const CircularProgressIndicator();
+                      },
                     ),
                   ),
                 ),
