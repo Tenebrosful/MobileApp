08be0f2c122c7840232b180b74f9856415399fd1
taniaolivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-30 10:48:57 +0200

    ajout des nouvelles routes

--- fllutter/lib/main.dart
+++ fllutter/lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:fllutter/components/commentaires.dart';
-import 'package:fllutter/components/geolocalisation.dart';
-import 'package:fllutter/components/one_event.dart';
+import 'package:fllutter/components/commentaires_joined_event.dart';
+import 'package:fllutter/components/commentaires_self_event.dart';
+import 'package:fllutter/components/creation.dart';
+import 'package:fllutter/components/geolocalisation_joined_event.dart';
+import 'package:fllutter/components/geolocalisation_self_event.dart';
+import 'package:fllutter/components/one_event_joined_event.dart';
+import 'package:fllutter/components/one_event_self_event.dart';
 import 'package:fllutter/components/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:fllutter/components/sign_in.dart' as connection;
-import 'components/creation.dart';
-import 'service/service_user.dart';
-import 'service/service_participant.dart';
-import 'service/service_auth.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,14 +33,21 @@ class MyApp extends StatelessWidget {
         '/profil': (context) => ProfilePage(),
         // When navigating to the "/ajout" route, build the AddEven widget.
         '/ajout': (context) => AddEven(),
-        // When navigating to the "/events" route, build the Service_event widget.
-        '/geolocalisation': (context) => Geolocalisation(),
-        // When navigating to the "/participants" route, build the Service_participant widget.
-        '/participants': (context) => Service_participant(),
-        // When navigating to the "/evenement" route, build the OneEvent widget.
-        '/evenement': (context) => OneEvent(),
-        // When navigating to the "/commentaires" route, build the Commentaires widget.
-        '/commentaires': (context) => Commentaires(),
+        // When navigating to the "geolocalisation/mes_evenements" route, build the GeolocalisationSelfEvent widget.
+        '/geolocalisation/mes_evenements': (context) =>
+            GeolocalisationSelfEvent(),
+        // When navigating to the "geolocalisation/evenements_participes" route, build the GeolocalisationJoinedEvent widget.
+        '/geolocalisation/evenements_participes': (context) =>
+            GeolocalisationJoinedEvent(),
+        // When navigating to the "/mon_evenement" route, build the OneEventSelfEvent widget.
+        '/mon_evenement': (context) => OneEventSelfEvent(),
+        // When navigating to the "/evenement_participe" route, build the OneEventJoinedEvent widget.
+        '/evenement_participe': (context) => OneEventJoinedEvent(),
+        // When navigating to the "/mon_evenement/commentaires" route, build the CommentairesSelfEvent widget.
+        '/mon_evenement/commentaires': (context) => CommentairesSelfEvent(),
+        // When navigating to the "/evemenent_participe/commentaires" route, build the CommentairesJoinedEvent widget.
+        '/evemenent_participe/commentaires': (context) =>
+            CommentairesJoinedEvent(),
       },
     );
   }
