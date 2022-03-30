2bd56767fcdfd0c70ecd7a09fbc26549617c2467
taniaolivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-30 10:41:11 +0200

    fix erreur route et erreur récupération de stockage

--- fllutter/lib/components/profile_page.dart
+++ fllutter/lib/components/profile_page.dart
@@ -1,11 +1,8 @@
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:fllutter/components/geolocalisation_self_event.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:fllutter/data/user_preferences.dart';
 import 'package:fllutter/components/appbar_widget.dart';
 import 'package:fllutter/components/profile_widget.dart';
-import 'package:fllutter/components/textfield_widget.dart';
 import 'package:fllutter/components/edit_profile_page.dart';
 import 'package:fllutter/themes.dart';
 import 'package:fllutter/src/users.dart';
@@ -22,61 +19,72 @@ class ProfilePageState extends State<ProfilePage> {
   static const String _title = 'Profil';
   String? token;
   String? owner_id;
+  final storage = FlutterSecureStorage();
+
+  Future<void> getToken() async {
+    token = await storage.read(key: "token");
+  }
+
+  Future<void> getUser() async {
+    owner_id = await storage.read(key: "id");
+  }
+
+  @override
+  void initState() {
+    getUser().then((owner_id) {
+      setState(() {
+        owner_id = owner_id;
+      });
+    });
+    getToken().then((token) {
+      setState(() {
+        token = token;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final storage = FlutterSecureStorage();
-
-    Future<void> _getUser() async {
-      token = await storage.read(key: "token");
-      owner_id = await storage.read(key: "id");
-    }
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('profil'),
-        backgroundColor: Colors.green[700],
+        title: const Text('Profil'),
+        backgroundColor: Colors.blue[600],
       ),
       body: FutureBuilder<user.User>(
         future: user.fetchUser(owner_id.toString(), token.toString()),
         builder: (context, snapshot) {
-          _getUser();
-          user.fetchUser(owner_id.toString(), token.toString());
-
           if (snapshot.hasData) {
             var user = snapshot.data!;
-            //return Text(user.toString());
             return ThemeProvider(
               initTheme: isDarkMode ? MyThemes.darkTheme : MyThemes.lightTheme,
               builder: (context, myTheme) {
-                return MaterialApp(
+                return /*MaterialApp(
                   title: _title,
                   theme: myTheme,
                   home: ThemeSwitchingArea(
                     child: Builder(
-                      builder: (context) => Scaffold(
-                        appBar: buildAppBar(context),
-                        body: ListView(
-                          physics: BouncingScrollPhysics(),
-                          children: [
-                            ProfileWidget(
-                              imagePath:
-                                  "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80",
-                              onClicked: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) => EditProfilePage()),
-                                );
-                              },
-                            ),
-                            const SizedBox(height: 24),
-                            buildName(user),
-                            const SizedBox(height: 48),
-                            buildId(user),
-                          ],
-                        ),
+                      builder: (context) => */
+                    Scaffold(
+                  body: ListView(
+                    physics: BouncingScrollPhysics(),
+                    children: [
+                      const SizedBox(height: 24),
+                      ProfileWidget(
+                        imagePath:
+                            "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80",
+                        onClicked: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => EditProfilePage()),
+                          );
+                        },
                       ),
-                    ),
+                      const SizedBox(height: 24),
+                      buildName(user),
+                      const SizedBox(height: 48),
+                      buildId(user),
+                    ],
                   ),
                 );
               },
@@ -112,7 +120,7 @@ Widget buildId(User user) => Container(
         children: [
           Text(
             'ID',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 16),
           Text(
