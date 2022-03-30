7cabd600716aa72d64ccfec58853842e7c484b11
taniaolivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-30 10:33:14 +0200

    ajout api fetch comments

--- fllutter/lib/src/comments.c.dart
+++ fllutter/lib/src/comments.c.dart
@@ -1,42 +1,40 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 part of 'comments.dart';
-
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-Author _$AuthorFromJson(Map<String, dynamic> json) => Author(
-    createdAt: json['createdAt'],
+Author _$AuthorFromJson(Map<String?, dynamic> json) => Author(
+    username: json['username'],
     id: json['id'],
     updatedAt: json['updatedAt'],
-    username: json['username']);
+    createdAt: json['createdAt']);
 
-Map<String, dynamic> _$AuthorToJson(Author instance) => <String, dynamic>{
-      'createdAt': instance.createdAt,
+Map<String?, dynamic> _$AuthorToJson(Author instance) => <String, dynamic>{
+      'username': instance.username,
       'id': instance.id,
       'updatedAt': instance.updatedAt,
-      'username': instance.username,
+      'createdAt': instance.createdAt
     };
 
 Comment _$CommentFromJson(Map<String?, dynamic> json) => Comment(
-      author_id: json['author_id'],
-      createdAt: json['createdAt'],
-      event_id: json['event_id'],
-      id: json['id'],
-      message: json['message'],
-      updatedAt: json['updatedAt'],
-      author: Author.fromJson(json['author'] as Map<String, dynamic>),
-    );
+    createdAt: json['createdAt'],
+    author_id: json['author_id'],
+    id: json['id'],
+    event_id: json['event_id'],
+    message: json['message'],
+    updatedAt: json['updatedAt'],
+    author: Author.fromJson(json['author'] as Map<String?, dynamic>));
 
 Map<String?, dynamic> _$CommentToJson(Comment instance) => <String?, dynamic>{
-      'author_id': instance.author_id,
       'createdAt': instance.createdAt,
-      'event_id': instance.event_id,
+      'author_id': instance.author_id,
       'id': instance.id,
-      'message': instance.message,
+      'event_id': instance.event_id,
       'updatedAt': instance.updatedAt,
-      'author': instance.author,
+      'message': instance.message,
+      'author': instance.author
     };
 
 Comments _$CommentsFromJson(Map<String?, dynamic> json) => Comments(
