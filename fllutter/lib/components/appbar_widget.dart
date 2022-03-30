d12985ab722a84893eb84c453f190ca740f6ae2e
taniaolivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-30 10:42:32 +0200

    remove import inutile

--- fllutter/lib/components/appbar_widget.dart
+++ fllutter/lib/components/appbar_widget.dart
@@ -2,8 +2,6 @@ import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fllutter/components/themes.dart';
-import 'package:fllutter/data/user_preferences.dart';
-import 'package:fllutter/components/geolocalisation.dart';
 
 AppBar buildAppBar(BuildContext context) {
   final isDarkMode = Theme.of(context).brightness == Brightness.dark;
