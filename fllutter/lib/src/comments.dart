067ae8bf096e7246c14034de904f60235b5fd175
taniaolivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-30 10:33:01 +0200

    ajout api fetch comments

--- fllutter/lib/src/comments.dart
+++ fllutter/lib/src/comments.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:json_annotation/json_annotation.dart';
-import 'package:uuid/uuid.dart';
 part 'comments.c.dart';
 
 @JsonSerializable()
@@ -13,37 +12,36 @@ class Author {
     required this.username,
   });
 
-  factory Author.fromJson(Map<String, dynamic> json) => _$AuthorFromJson(json);
-  Map<String, dynamic> toJson() => _$AuthorToJson(this);
-  final String createdAt;
-  final String id;
-  final String updatedAt;
-  final String username;
+  factory Author.fromJson(Map<String?, dynamic> json) => _$AuthorFromJson(json);
+  Map<String?, dynamic> toJson() => _$AuthorToJson(this);
+  final String? createdAt;
+  final String? id;
+  final String? updatedAt;
+  final String? username;
 }
 
 @JsonSerializable()
 class Comment {
-  Comment({
-    required this.author_id,
-    required this.createdAt,
-    required this.event_id,
-    required this.id,
-    required this.message,
-    required this.updatedAt,
-    required this.author,
-  });
+  Comment(
+      {required this.author_id,
+      required this.createdAt,
+      required this.event_id,
+      required this.id,
+      required this.message,
+      required this.updatedAt,
+      required this.author});
 
   factory Comment.fromJson(Map<String?, dynamic> json) =>
       _$CommentFromJson(json);
   Map<String?, dynamic> toJson() => _$CommentToJson(this);
 
-  final String? author_id;
+  final Author author;
   final String? createdAt;
+  final String? author_id;
   final String? event_id;
   final String? id;
   final String? message;
   final String? updatedAt;
-  final Author author;
 }
 
 @JsonSerializable()
@@ -61,17 +59,16 @@ class Comments {
   final int count;
 }
 
-Future<Comments> fetchEvent(String id) async {
-  final response = await http.get(Uri.parse(
-      'http://docketu.iutnc.univ-lorraine.fr:62460/api/event/' +
-          id +
-          '/comments?embedAuthor=true'));
+Future<Comments> fetchComments(String token, String id) async {
+  final response = await http.get(
+      Uri.parse(
+          'http://docketu.iutnc.univ-lorraine.fr:62461/api/event/${id}/comments?embedAuthor=true'),
+      headers: <String, String>{'authorization': token});
 
   if (response.statusCode == 200) {
     var res = json.decode(response.body);
-
     return Comments.fromJson(res);
   } else {
-    throw Exception('Failed to load event');
+    throw Exception('Failed to load comments');
   }
 }
